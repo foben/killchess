cd15afbd2baaae2aac9163b73510c537f94dbfdf
Felix Obenauer <f.obenauer@gmail.com>
2014-02-09 13:18:49 +0100

    Refactored State Handling

--- controller.py
+++ controller.py
@@ -2,19 +2,48 @@ import pygame
 from board import Board
 from entity import *
 
+class State:
+    def __init__(self, context):
+        self.context = context
+    def click_empty_tile(self, position):
+        print "%s : empty" % (position, )
+    def click_occupied_tile(self, position, entities):
+        print "%s : occupied with %s" % (position, entities)
+        entity = entities[0]
+        e_movements = entity.get_movements()
+        movement_targets = self.context.get_places(e_movements, position)
+        self.context.state = UnitSelectedForMovementState\
+                (self.context, entity, movement_targets)
+
+class UnitSelectedForMovementState(State):
+    def __init__(self, context, entity, movement_targets):
+        State.__init__(self, context)
+        self.entity = entity
+        self.movement_targets = movement_targets
+        for mt in movement_targets:
+            marker_position = self.context.board.get_tile(mt).rect.center
+            self.context.marker_group.add(Marker(marker_position))
+
+    def click_empty_tile(self, position):
+        if position in self.movement_targets:
+            self.context.board.move_entity(self.entity, position)
+            self.context.state = State(self.context)
+            self.context.marker_group.empty()
+        else:
+            print "Not reachable!"
+
+    def click_occupied_tile(self, position, entities):
+        print "Can't move here, already occupied!"
+
 class Controller:
     def __init__(self, screen, board):
         self.screen = screen
         self.board = board
         self.highlighter, self.highlighterrect = \
                 futils.load_image('images/tile_hover.png', (255, 0, 255))
-        self.state = {'state': 'selection', 'substate': None}
         self.marker_group = pygame.sprite.Group()
+        self.state = State(self)
     
-
-    def update(self):
-        return
-
     def trigger_draw(self):
         self.board.draw(self.screen)
         self.screen.blit(self.highlighter, self.highlighterrect)
@@ -24,58 +53,23 @@ class Controller:
         self.highlighterrect.center = newpos 
 
     def process(self, mpos, clicked):
-        hovered_tile = self.board.get_tile_from_position(mpos)
+        hovered_tile = None
         position = None
+        hovered_tile = self.board.get_tile_from_position(mpos)
         if hovered_tile:
             position = hovered_tile.pos
             self.adjust_highlighter(hovered_tile.rect.center)
-        
         if clicked and position:
-            self.process_click_on_tile(position)
-        
-    def process_click_on_tile(self, position):
-        #print "before:  %s" % self.state
-        if self.state['state'] == "selection" and not self.state['substate']:
-            self.process_selection_click(position)
-        elif self.state['state'] == "selection" and self.state['substate'] == 'unit':
-            self.process_unit_selected_click(position)
-        #print "after:  %s" % self.state
-
-    
-    def process_unit_selected_click(self, position):
-        valids = self.state['targets']
-        self.marker_group.empty()
-        if position in valids:
-            self.board.move_entity(self.state['unit'], position)
-            self.clear_state()
-        else:
-            self.clear_state()
-
-    def process_selection_click(self, position):
-        self.marker_group.empty()
-        e_group = self.board.get_entities_on_tile(position)
-        e_list = e_group.sprites()
-        if len(e_list) > 1:
-            print "MORE THAN ONE ENTITY HERE!!!!"
-            self.clear_state()
-            return
-        if len(e_list) < 1:
-            print "No entity here"
-            self.clear_state()
-            return
-        entity = e_list[0]
-        mv = entity.get_movements()
-        places = self.get_places(mv, position)
-        for p in places:
-            marker_position = self.board.get_tile(p).rect.center
-            self.marker_group.add(Marker(marker_position))
-        self.state['targets'] = places
-        self.state['unit'] = entity
-        self.state['substate'] = 'unit'
-
-    def clear_state(self):
-        self.state = {'state': 'selection', 'substate':None }
-
+            e_group = self.board.get_entities_on_tile(position)
+            e_list = e_group.sprites()
+            if len(e_list) < 1:
+                self.state.click_empty_tile(position)
+            elif len(e_list) == 1:
+                self.state.click_occupied_tile(position, e_list)
+            elif len(e_list) > 1:
+                print "MORE THAN ONE ENTITY HERE!!!!"
+                return
+            
     def get_places(self, places, position):
         candidates = [ tuple(map(lambda x, y: x+y, p, position)) for p in places ]
         result = list()
@@ -91,12 +85,8 @@ class Controller:
         print "%s invalid positions removed" % failcount
         return result
 
-class State:
-    def click_empty_tile(self, position):
-        print "%s : empty" % position
-    def click_occupied_tile(self, position, entities):
-        print "%s : occupied with %s" % (position, entities)
-
+    def update(self):
+        return
 
 class Marker(pygame.sprite.Sprite):
     def __init__(self, position):
